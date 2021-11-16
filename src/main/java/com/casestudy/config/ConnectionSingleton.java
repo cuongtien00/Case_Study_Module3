d3f02d67f543ad86438b0ba31637240109bae79d
unknown <phamvietcuong12@gmail.com>
2021-11-16 23:51:23 +0700

    done role service

--- src/main/java/com/casestudy/config/ConnectionSingleton.java
+++ src/main/java/com/casestudy/config/ConnectionSingleton.java
@@ -11,7 +11,7 @@ public class ConnectionSingleton {
     }
     public static final String URL = "jdbc:mysql://localhost:3306/case_study_module3";
     public static final String USER = "root";
-    public static final String PASSWORD = "Cuongtien1809";
+    public static final String PASSWORD = "Hocbaidi1212";
     public static Connection getConnection(){
         if(connection==null){
             try {

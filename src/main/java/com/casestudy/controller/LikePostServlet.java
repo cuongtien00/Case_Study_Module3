bf2e3c0b897afa5a6a5272411133e278fe90749a
cuongtien00 <cuonghiep000@gmail.com>
2021-11-17 19:47:40 +0700

    add LikePost Servlet

--- src/main/java/com/casestudy/controller/LikePostServlet.java
+++ src/main/java/com/casestudy/controller/LikePostServlet.java
@@ -9,11 +9,41 @@ import java.io.IOException;
 public class LikePostServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                break;
+        }
     }
 }
